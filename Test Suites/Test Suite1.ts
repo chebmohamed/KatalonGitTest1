774fc1878924cc627f8efcbbceb6fbcef5e45f45
cheb_mohamed <cheb_mohamed@hotmail.com>
2021-03-13 11:30:59 +0100

    next changes

---
+++ Test Suites/Test Suite1.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Test Suite1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>2a6d655e-025b-440c-9677-ad11b157e011</testSuiteGuid>
+</TestSuiteEntity>
